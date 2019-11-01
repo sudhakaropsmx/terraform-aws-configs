3aa79959985f195f68ebb1974ad827495ee79366
Sudhakar Guvvalapall <sudhakar@opsmx.in>
2019-11-01 19:19:44 +0530

    updated with default

--- dev-abc-xyz/variables.tf
+++ dev-abc-xyz/variables.tf
@@ -1,31 +1,34 @@
-variable "region" {}
+variable "region" {default = "us-west-2"}
 variable "zone" {
     type = "list"
+    default = ["us-west-2a", "us-west-2b"]
 }
 variable "security_groups" {
-    type = "list"
+   type = "list"
+   default = ["sg-0dfc7e6c498a806bf"] 
+   
 }
-variable "date" {}
-variable "subnet1" {}
-variable "subnet2" {}
-variable "name" {}
-variable "instancetype" {}
-variable "vpc_id" {}
-variable "keypair" {}
-variable "instance_profile_name" {}
-variable "minsize" {}
-variable "maxsize" {}
-variable "remotetags" {}
-variable "hc_grace" {}
-variable "remotevarpath" {}
-variable "desired_capacity" {}
+variable "date" {default = "2019-10-23"}
+variable "subnet1" {default = "subnet-0a7a3e106bb8e0028"}
+variable "subnet2" {default = "subnet-043ab52cc73a3fe51"}
+variable "name" {default = "dev-abc-xyz"}
+variable "instancetype" {default = "t2.medium"}
+variable "vpc_id" {default = "vpc-0c496debf358f0c3b"}
+variable "keypair" {default = "keypairtest1"}
+variable "instance_profile_name" {default = "arn:aws:iam::732813442182:role/ec2-instance"}
+variable "minsize" {default = "1"}
+variable "maxsize" {default = "2"}
+variable "remotetags" {default = "infra,app-java"}
+variable "hc_grace" {default = "300"}
+variable "remotevarpath" {default = "arp-workspace-activity/dev-vars.yml"}
+variable "desired_capacity" {default = "1"}
 
-variable "appname" {}
-variable "application_framework" {}
-variable "application_owner" {}
-variable "application_role" {}
-variable "product_name" {}
-variable "product_subcategory" {}
+variable "appname" {default = "try1"}
+variable "application_framework" {default = "try1"}
+variable "application_owner" {default = "try1"}
+variable "application_role" {default = "try1"}
+variable "product_name" {default = "try1"}
+variable "product_subcategory" {default = "try1"}
 
 locals {
   common_tags = {
